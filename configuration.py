efe5939f871ce71883fd7c7667f3dcb17685ed31
Karina Luvianos <luvianos.karina@outlook.com>
2026-03-30 19:02:27 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-8eb0ff8e-e6bd-43f7-a8b6-c375cc503f59.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
